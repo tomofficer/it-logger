64dd9ac41210fadf1832f2fb3f8e8692730c0615
Tom Officer <thomasofficerjr@gmail.com>
2021-11-06 01:28:31 -0700

    updated state

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -15,6 +15,8 @@ import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types'
 //     };
 // };
 
+
+//get logs from server
 export const getLogs = () => async dispatch => {
         try {
             setLoading();
--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,30 +1,23 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
 import LogItem from './LogItem';
 import Preloader from '../layout/Preloader';
+import PropTypes from 'prop-types'
+import { getLogs } from '../../actions/logActions'
 
 
-const Logs = () => {
 
-    const [logs, setLogs] = useState([]);
-    const [loading, setLoading] = useState(false);
+const Logs = ({ log: { logs, loading }, getLogs }) => {
+
 
     useEffect(() => {
         getLogs();
         //eslint-disable-next-line
     }, []);
 
-    const getLogs = async () => {
-        setLoading(true);
-        const res = await fetch('/logs');
-        const data = await res.json();
-
-        setLogs(data);
-        setLoading(false);
-    };
-
-    if (loading) {
-        return <Preloader />
-    };
+    if (loading || logs === null) {
+        return <Preloader />;
+    }
 
 
     return (
@@ -35,10 +28,22 @@ const Logs = () => {
             {!loading && logs.length === 0 ? (
                 <p className="center">No logs to show...</p>
             ) : (
-                    logs.map(log => <LogItem log={log} key={log.id}/>)
+                logs.map(log => <LogItem log={log} key={log.id} />)
             )}
         </ul>
     )
+};
+
+Logs.propTypes = {
+    log: PropTypes.object.isRequired
 }
 
-export default Logs
+
+const mapStateToProps = state => ({
+    log: state.log
+});
+
+export default connect(mapStateToProps, { getLogs })(Logs);
+
+//alternate option for mapStateToProps :
+// logs: state.log.logs, loading: state.log.loading
